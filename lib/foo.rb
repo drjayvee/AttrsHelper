2bbc82d20d312884937b92605ddd75039457772e
Jeroen Versteeg <firm.grip0126@fastmail.com>
2025-11-06 10:35:51 +0100

    Simplify code even more

--- lib/foo.rb
+++ lib/foo.rb
@@ -1,36 +1,34 @@
 # frozen_string_literal: true
 
-module Foo
-  class Good
-    def self.attrs(*attrs)
-      attr_reader(*attrs) # not necessary to reproduce the bug
+class Good
+  def self.attrs(*attrs)
+    attr_reader(*attrs) # not necessary to reproduce the bug
 
-      define_method :initialize do |*args|
-        puts "Base:initialize(#{args.inspect})"
-        attrs.each_with_index { |attr, i| instance_variable_set "@#{attr}", args[i] }
-      end
-    end
-
-    attrs :a
-  end
-
-  module AttrsHelper
-    def attrs(*attrs)
-      attr_reader(*attrs)
-
-      define_method :initialize do |*args|
-        puts "Pase:initialize(#{args.inspect})"
-        attrs.each_with_index { |attr, i| instance_variable_set "@#{attr}", args[i] }
-      end
+    define_method :initialize do |*args|
+      puts "Base:initialize(#{args.inspect})"
+      attrs.each_with_index { |attr, i| instance_variable_set "@#{attr}", args[i] }
     end
   end
 
-  class Bad
-    extend AttrsHelper
+  attrs :a
+end
 
-    attrs :a
+module AttrsHelper
+  def attrs(*attrs)
+    attr_reader(*attrs)
+
+    define_method :initialize do |*args|
+      puts "Pase:initialize(#{args.inspect})"
+      attrs.each_with_index { |attr, i| instance_variable_set "@#{attr}", args[i] }
+    end
   end
 end
 
-Foo::Good.new 'a'
-Foo::Bad.new 'a'
+class Bad
+  extend AttrsHelper
+
+  attrs :a
+end
+
+Good.new 'a'
+Bad.new 'a'
